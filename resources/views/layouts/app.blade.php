90541eec00b831db201b90ef292b771fa29b3051
Muhammad Taufik Mubarok <taufik.apt.1983@gmail.com>
2026-05-26 13:12:37 +0700

    remove vite use tailwind cdn

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,8 +12,8 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
+        <script src="https://cdn.tailwindcss.com"></script>
+        </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
             @include('layouts.navigation')
